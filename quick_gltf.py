6fa6fe8e2f65f53d5d13fe37e3c6290c6a533903
ThatCoolCoder <ThatCoolCoder@users.noreply.github.com>
2022-07-11 16:42:38 +0930

    Finally get blender exporter working on nested objects

--- quick_gltf.py
+++ quick_gltf.py
@@ -7,41 +7,69 @@ bl_info = {
 keymaps = []
 
 import bpy
+import mathutils
 import os
 
+# Yes it's horribly messy, but it's a tricky task.
+
 class QuickGltf(bpy.types.Operator):
-    '''Export an object. If it's a curve converts it to a mesh first'''
+    '''Exports all selected objects and their children into a GLTF file. Converts curves to meshes first'''
+
     bl_idname = 'object.quick_gltf'
     bl_label = 'Quick GLTF'
     bl_options = {'REGISTER', 'UNDO'}
 
     def execute(self, context):
-        all_selected = bpy.context.selected_objects
-
+        # Get selected stuff
+        all_selected = bpy.context.selected_objects.copy()
         if len(all_selected) == 0:
             self.report_warning('No objects selected')
             return {"CANCELLED"}
 
+        # Recursively select their children
+        to_clone = []
         for obj in all_selected:
-            obj.select_set(True)
-        bpy.ops.object.select_grouped(type='CHILDREN_RECURSIVE')
+            to_clone += self.select_recursive(obj)
 
-        all_selected = bpy.context.selected_objects
-
+        # Figure out where to save
         blend_file_path = bpy.data.filepath
         directory = os.path.dirname(blend_file_path)
-        target_path = os.path.join(directory, all_selected[0].name + '.glb')
+        target_path = os.path.join(directory, to_clone[0].name + '.glb')
 
+        # Figure out which objects are "roots" and need to be moved to zero position after cloning is done
+        # (Must not move the children as they're moved along with the parents)
+        # (Has effect of recursion because we recursively select children earlier)
+        roots = []
+        for obj in to_clone:
+            if obj.parent not in to_clone:
+                roots.append(obj)
+
+        # Clone/convert all objects
         created_objects = []
+        old_name_to_cloned_name = {}
+        new_roots = []
+        for obj in to_clone:
 
-        for obj in all_selected:
             cloned = self.clone_for_export(context, obj)
-            cloned.location.x = 0
-            cloned.location.y = 0
-            cloned.location.z = 0
-            created_objects.append(cloned)
+            if obj in roots:
+                new_roots.append(cloned)
+            else:
+                # Update parent of creatd object
+                saved_matrix = cloned.matrix_world
+                cloned.parent = bpy.data.objects[old_name_to_cloned_name[obj.parent.name]]
+                cloned.matrix_world = saved_matrix
 
-        for obj in created_objects:
+                
+            created_objects.append(cloned)
+            # Keep track of what this used to be for re-rooting
+            old_name_to_cloned_name[obj.name] = cloned.name
+
+        # Prepare objects for export
+        for (idx, obj) in enumerate(created_objects):
+            # Move roots to zero position so they're not offset in the exported file
+            if obj in new_roots:
+                obj.location = mathutils.Vector((0, 0, 0))
+            # Select everything
             obj.select_set(True)
 
         bpy.ops.export_scene.gltf(filepath=target_path, use_selection=True)
@@ -84,11 +112,23 @@ class QuickGltf(bpy.types.Operator):
     #         selected.location = old_location
 
     #     return {'FINISHED'}
+
+    def pos_relative_to_root(self, obj, roots):
+        current_obj = obj
+        sub_total_position = mathutils.Vector((0, 0, 0))
+        print('rooting', obj.name)
+        while current_obj not in roots:
+            print('sub: ', current_obj.matrix_local.translation)
+            sub_total_position += current_obj.matrix_local.translation
+            current_obj = current_obj.parent
+        return sub_total_position
     
     def clone_for_export(self, context, obj):
         if obj.type == 'CURVE':
             return self.curve_to_mesh(context, obj)
         else:
+            for already_selected in bpy.context.selected_objects:
+                already_selected.select_set(False)
             obj.select_set(True)
             bpy.ops.object.duplicate()
             return context.selected_objects[0]
@@ -110,6 +150,13 @@ class QuickGltf(bpy.types.Operator):
 
         return new_obj
     
+    def select_recursive(self, obj):
+        obj.select_set(True)
+        result = [obj]
+        for child in obj.children:
+            result += self.select_recursive(child)
+        return result
+
     def report_warning(self, message):
         self.report({"WARNING"}, message)
     
